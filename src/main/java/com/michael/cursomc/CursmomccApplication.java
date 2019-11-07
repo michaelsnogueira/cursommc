c1d7b966c0f86a46da9a2f5fdd4865f458dc2375
Michael Nogueira <michaelsnogueira@gmail.com>
2019-11-07 05:44:44 -0300

    Projeto Criado

---
+++ src/main/java/com/michael/cursomc/CursmomccApplication.java
@@ -0,0 +1,13 @@
+package com.michael.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursmomccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursmomccApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/michael/cursomc/CursmomccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michael.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursmomccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
