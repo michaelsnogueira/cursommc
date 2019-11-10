8ce2c500ba96f77d228a2d1116f226e545b9c830
Michael Nogueira <michaelsnogueira@gmail.com>
2019-11-10 20:19:43 -0300

    Estado e Cidade

---
+++ src/main/java/com/michael/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.michael.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.michael.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+
+}
