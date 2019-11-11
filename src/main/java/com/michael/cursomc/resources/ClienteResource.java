d006d1992e0c6f00f8aa79228eb51411900ed50f
Michael Nogueira <michaelsnogueira@gmail.com>
2019-11-11 07:12:00 -0300

    Endpoins/clientes/{id} disponivel

---
+++ src/main/java/com/michael/cursomc/resources/ClienteResource.java
@@ -0,0 +1,31 @@
+package com.michael.cursomc.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.michael.cursomc.domain.Cliente;
+import com.michael.cursomc.repositories.EnderecoRepository;
+import com.michael.cursomc.services.ClienteService;
+
+@RestController
+@RequestMapping(value = "/clientes")
+public class ClienteResource {
+	
+	@Autowired
+	private ClienteService service;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Cliente obj = service.find(id);
+		return ResponseEntity.ok().body(obj);
+		
+	}
+
+}
